f61c930a62274b7a14ff338ffa5b6606f1937b26
Darius Costolas <dariuscostolas@yahoo.com>
2017-05-04 15:27:02 +0300

    Implemented Graphics and sprites

---
+++ ChessEx2/utility/Sprite.cpp
@@ -0,0 +1,9 @@
+//
+//  Sprite.cpp
+//  ChessEx2
+//
+//  Created by Darius Costolas on 5/4/17.
+//  Copyright © 2017 Darius Costolas. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ChessEx2/utility/Sprite.h
@@ -0,0 +1,13 @@
+//
+//  Sprite.h
+//  ChessEx2
+//
+//  Created by Darius Costolas on 5/4/17.
+//  Copyright © 2017 Darius Costolas. All rights reserved.
+//
+
+#ifndef Sprite_h
+#define Sprite_h
+
+
+#endif /* Sprite_h */
